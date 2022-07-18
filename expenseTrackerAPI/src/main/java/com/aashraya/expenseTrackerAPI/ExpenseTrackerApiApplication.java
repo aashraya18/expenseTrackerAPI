f07cbe1bc605f738367c3c200ba88305fc82cb09
Aashraya Singal <aashraya18@gmail.com>
2022-07-19 01:57:54 +0530

    Cors updated

--- expenseTrackerAPI/src/main/java/com/aashraya/expenseTrackerAPI/ExpenseTrackerApiApplication.java
+++ expenseTrackerAPI/src/main/java/com/aashraya/expenseTrackerAPI/ExpenseTrackerApiApplication.java
@@ -1,16 +1,31 @@
 package com.aashraya.expenseTrackerAPI;
 
 import com.aashraya.expenseTrackerAPI.filter.AuthFilter;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter());
+		registrationBean.setOrder(0);
+		return registrationBean;
 	}
 	@Bean
 	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
